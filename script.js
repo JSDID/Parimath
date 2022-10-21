0c68f9463d98eabc472e7668f43a1e39fb0d9e33
Olexander Dolinsky <102165823+JSDID@users.noreply.github.com>
2022-10-21 15:41:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.wheel').addEventListener('click', start);
+const inside = document.querySelector('.inside');
+
+function start() {
+  inside.classList.add('varian');
+  setTimeout(() => {
+    alert('Підпишись на повідомлення від Parimatch. Обіцяємо, спаму не буде!');
+    inside.classList.remove('varian-1');
+  }, 4000);
+}
+
+// https://start.parimatch.com/casino/2/reg/1/ua/?tid=pr_a9b1_cas_cleo_propeller&prism_click_id=7705bef8-3e0b-4d81-adc5-74317a946fdb&adtag=ud7cec0a0622e6304_a_b1854790424_c1535738190_d1476818913_e1681662599&brand_id=1&brand=UA+%2F+COM+%2F+A+%2F+start.parimatch.com&landing_path=%2Fcasino%2F2%2Freg%2F1%2Fua%2F
